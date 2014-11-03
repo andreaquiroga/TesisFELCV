e8704d38df709a93fc01ef4fa4a70c8252a97999
andreaquiroga <andy.afqy@gmail.com>
2014-11-03 12:19:53 -0400

    arreglando mailer errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,18 @@ Felcv::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.assets.compile = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'gmail.com',
+    :user_name => 'felcv.cochabamba@gmail.com',
+    :password => 'policiaboliviana',
+}
 end
